7319f34ede0523842aff3764cf0ae62a231e9183
Søren Lund <lund14@gmail.com>
2022-04-04 20:38:03 +0200

    Implement ButtonToolbarTagHelper

---
+++ src/TagHelpers/Components/Buttons/ButtonToolbarTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers.Components.Buttons;
+
+/// <summary>
+/// Combine sets of button groups into button toolbars for more complex components.
+/// Use utility classes as needed to space out groups, buttons, and more.
+/// </summary>
+[HtmlTargetElement("btn-toolbar", ParentTag = null)]
+public class ButtonToolbarTagHelper : BootstrapTagHelperBase
+{
+    /// <summary>
+    /// Toolbars should be given an explicit label, as most assistive technologies will otherwise not announce them.
+    /// </summary>
+    [HtmlAttributeName("label")]
+    public string? Label { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.Attributes.SetAttribute("role", "toolbar");
+
+        if (!string.IsNullOrEmpty(Label))
+            output.Attributes.SetAttribute("aria-label", Label);
+
+        output.AddCssClasses(ClassNames.ButtonToolbars.ButtonToolbar);
+    }
+}
---
+++ tests/TagHelpers.UnitTests/Components/Buttons/ButtonToolbarTagHelperTests.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using TagHelpers.Components.Buttons;
+using Xunit;
+
+namespace TagHelpers.UnitTests.Components.Buttons;
+
+public class ButtonToolbarTagHelperTests
+{
+    public static TheoryData<TagHelperAttributeList, TagHelperAttributeList, TagHelperAttributeList> TestDataSet
+    {
+        get
+        {
+            return new TheoryData<TagHelperAttributeList, TagHelperAttributeList, TagHelperAttributeList>
+            {
+                {
+                    new TagHelperAttributeList
+                    {
+                    },
+                    new TagHelperAttributeList
+                    {
+                    },
+                    new TagHelperAttributeList
+                    {
+                        { "role", "toolbar" },
+                        { "class", "btn-toolbar" },
+                    }
+                },
+                {
+                    new TagHelperAttributeList
+                    {
+                        { "label", "Testing toolbar SR labels" }
+                    },
+                    new TagHelperAttributeList
+                    {
+                        { "id", "MyToolbar" },
+                    },
+                    new TagHelperAttributeList
+                    {
+                        { "id", "MyToolbar" },
+                        { "role", "toolbar" },
+                        { "aria-label", "Testing toolbar SR labels" },
+                        { "class", "btn-toolbar" },
+                    }
+                }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDataSet))]
+    public void Process_GeneratesExpectedOutput(
+        TagHelperAttributeList contextAttributes,
+        TagHelperAttributeList outputAttributes,
+        TagHelperAttributeList expectedAttributes)
+    {
+        // Arrange
+        const string exptectedContent = "someContent";
+
+        var tagHelperContext = TagHelperTesting.CreateTagHelperContext("btn-toolbar", contextAttributes);
+        var tagHelperOutput = TagHelperTesting.CreateTagHelperOutput("btn-toolbar", outputAttributes);
+
+        tagHelperOutput.Content.SetContent(exptectedContent);
+
+        var buttonToolbarTagHelper = new ButtonToolbarTagHelper()
+        {
+            Label = contextAttributes.TryGetAttribute("label", out var label) ? label.Value?.ToString() : null,
+        };
+
+        // Act
+        buttonToolbarTagHelper.Process(tagHelperContext, tagHelperOutput);
+
+        // Assert
+        tagHelperOutput.TagName.Should().Be("div");
+        tagHelperOutput.TagMode.Should().Be(TagMode.StartTagAndEndTag);
+        tagHelperOutput.Content.GetContent().Should().Be(exptectedContent);
+        tagHelperOutput.Attributes.Count.Should().Be(expectedAttributes.Count);
+        foreach (var expectedAttribute in expectedAttributes)
+            tagHelperOutput.Attributes.Should().ContainSingle(attribute => attribute.Name == expectedAttribute.Name).Which.Should().BeEquivalentTo(expectedAttribute);
+    }
+}
