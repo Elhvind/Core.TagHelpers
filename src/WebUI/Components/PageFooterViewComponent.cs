7523d6b0d6edf463f273a28f58d2c539dcd96fca
Søren Lund <sl@dnmh.dk>
2022-04-08 09:13:24 +0200

    Create ViewComponents for Header and Footer

---
+++ src/WebUI/Components/PageFooterViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Components;
+
+public class PageFooterViewModel
+{
+
+}
+
+public class PageFooterViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var viewModel = new PageFooterViewModel();
+        return View("PageFooter", viewModel);
+    }
+}
---
+++ src/WebUI/Components/PageHeaderViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Components;
+
+public class PageHeaderViewModel
+{
+
+}
+
+public class PageHeaderViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var viewModel = new PageHeaderViewModel();
+        return View("PageHeader", viewModel);
+    }
+}
